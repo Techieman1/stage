62e05c521d205711a8c382d89c6d1ca5177a18ba
Abdulazeez Abdulsamad <abdulazeezabdulsamad16@gmail.com>
2023-09-07 17:38:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const moment = require('moment');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/info', (req, res) => {
+  const { slack_name, track } = req.query;
+
+  if (!slack_name || !track) {
+    return res.status(400).json({ error: 'Missing required parameters' });
+  }
+ 
+  const current_day = moment().format('dddd');
+  const utc_time = moment.utc().format();
+  
+  const github_file_url = 'https://github.com/yourusername/yourrepository/blob/master/yourfile.js'; // Replace with your actual GitHub URLs
+  const github_repo_url = 'https://github.com/Techieman1/stage'
+
+  
+  res.status(200).json({
+    slack_name,
+    current_day,
+    utc_time,
+    track,
+    github_file_url,
+    github_repo_url,
+    status_code:200,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
