392c365a188a0b3da61c7da013a2567e74714851
Abdulazeez Abdulsamad <abdulazeezabdulsamad16@gmail.com>
2023-09-14 07:20:57 +0100

    second task

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-const express = require('express');
+const express = require("express")
+const {json} = require("express")
+const connect = require("./database/database")
+const app = express()
 const moment = require('moment');
-const app = express();
-const port = process.env.PORT || 3000;
+const personRouter = require("./router/personRouter")
+const port =   process.env.PORT || 4000
+connect()
+app.use(json())
+app.use("/", personRouter)
 
-app.get('/api', (req, res) => {
-  const { slack_name, track } = req.query;
-
-  if (!slack_name || !track) {
-    return res.status(400).json({ error: 'Missing required parameters' });
-  }
- 
-  const current_day = moment().format('dddd');
-  const utc_time = moment.utc().format();
+app.get('/', (req, res) => {
+    const { slack_name, track } = req.query;
   
-  const github_file_url = 'https://github.com/Techieman1/stage/blob/main/index.js';
-  const github_repo_url = 'https://github.com/Techieman1/stage'
-
+    if (!slack_name || !track) {
+      return res.status(400).json({ error: 'Missing required parameters' });
+    }
+   
+    const current_day = moment().format('dddd');
+    const utc_time = moment.utc().format();
+    
+    const github_file_url = 'https://github.com/Techieman1/stage/blob/main/index.js';
+    const github_repo_url = 'https://github.com/Techieman1/stage'
   
-  res.status(200).json({
-    slack_name,
-    current_day,
-    utc_time,
-    track,
-    github_file_url,
-    github_repo_url,
-    status_code:200,
+    
+    res.status(200).json({
+      slack_name,
+      current_day,
+      utc_time,
+      track,
+      github_file_url,
+      github_repo_url,
+      status_code:200,
+    });
   });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  
+app.listen(port, ()=>{
+    console.log(`listening to port ${port}`)
+})
